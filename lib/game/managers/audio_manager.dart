be222264bdb3fc7103350728dabbe133732429ca
ZEINadli <1123150028@global.ac.id>
2026-02-19 20:22:06 +0700

    membuat func play, stop, pause, resume, play with volume, toggle, enable, disable dispose music dan sfx

--- lib/game/managers/audio_manager.dart
+++ lib/game/managers/audio_manager.dart
@@ -26,4 +26,117 @@ class AudioManager {
       print('Error initializing audio: $e');
     }
   }
+
+  void playBackgroundMusic() {
+    if (_isMusicEnabled) {
+      try {
+        FlameAudio.bgm.play('music/background_music.mp3', volume: _musicVolume);
+      } catch (e) {
+        print('Error playing background music: $e');
+      }
+    }
+  }
+
+  void stopBackgroundMusic() {
+    try {
+      FlameAudio.bgm.stop();
+    } catch (e) {
+      print('Error stopping background music: $e');
+    }
+  }
+
+  void pauseBackgroundMusic() {
+    try {
+      FlameAudio.bgm.pause();
+    } catch (e) {
+      print('Error pausing background music: $e');
+    }
+  }
+
+  void resumeBackgroundMusic() {
+    if (_isMusicEnabled) {
+      try {
+        FlameAudio.bgm.resume();
+      } catch (e) {
+        print('Error resuming background music: $e');
+      }
+    }
+  }
+
+  void playSfx(String fileName) {
+    if (_isSfxEnabled) {
+      try {
+        FlameAudio.play('sfx/$fileName', volume: _sfxVolume);
+      } catch (e) {
+        print('Error playing SFX: $e');
+      }
+    }
+  }
+
+  void playSfxWithVolume(String fileName, double volume) {
+    if (_isSfxEnabled) {
+      try {
+        final adjustedVolume = (volume * _sfxVolume).clamp(0.0, 1.0);
+        FlameAudio.play('sfx/$fileName', volume: adjustedVolume);
+      } catch (e) {
+        print('Error playing SFX with volume: $e');
+      }
+    }
+  }
+
+  void setMusicVolume(double volume) {
+    _musicVolume = volume.clamp(0.0, 1.0);
+    try {
+      FlameAudio.bgm.audioPlayer.setVolume(_musicVolume);
+    } catch (e) {
+      print('Error setting music volume: $e');
+    }
+  }
+
+  void setSfxVolume(double volume) {
+    _sfxVolume = volume.clamp(0.0, 1.0);
+  }
+
+  void toggleMusic() {
+    _isMusicEnabled = !_isMusicEnabled;
+    if (_isMusicEnabled) {
+      resumeBackgroundMusic();
+    } else {
+      pauseBackgroundMusic();
+    }
+  }
+
+  void toggleSfx() {
+    _isSfxEnabled = !_isSfxEnabled;
+  }
+
+  void enableMusic() {
+    if (!_isMusicEnabled) {
+      _isMusicEnabled = true;
+      resumeBackgroundMusic();
+    }
+  }
+
+  void disableMusic() {
+    if (_isMusicEnabled) {
+      _isMusicEnabled = false;
+      pauseBackgroundMusic();
+    }
+  }
+
+  void enableSfx() {
+    _isSfxEnabled = true;
+  }
+
+  void disableSfx() {
+    _isSfxEnabled = false;
+  }
+
+  void dispose() {
+    try {
+      FlameAudio.bgm.dispose();
+    } catch (e) {
+      print('Error disposing audio: $e');
+    }
+  }
 }
