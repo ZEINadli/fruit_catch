564a851104f83e1662a181fa1b4a2267bbd26040
ZEINadli <1123150028@global.ac.id>
2026-02-19 20:07:40 +0700

    panggil yang ada di class fruit catcher game ke main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:fruit_catch/game/fruit_catcher_game.dart';
 
-void main() {
+void main() async {
   runApp(MyApp());
 }
 
@@ -10,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Catch',
+      title: 'FruitCatch',
       home: GameScreen(),
       debugShowCheckedModeBanner: false,
     );
@@ -25,11 +27,12 @@ class GameScreen extends StatefulWidget {
 }
 
 class _GameScreenState extends State<GameScreen> {
-  final ValueNotifier<int> counter = ValueNotifier(1);
+  late FruitCatcherGame game;
 
   @override
   void initState() {
     super.initState();
+    game = FruitCatcherGame();
   }
 
   @override
@@ -37,61 +40,51 @@ class _GameScreenState extends State<GameScreen> {
     return Scaffold(
       body: Column(
         children: [
-          Expanded(
-            child: Stack(
-              children: [
-                Positioned(
-                  top: 50,
-                  left: 20,
-                  child: Container(
-                    padding: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      color: Colors.black54,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-            
-                    child: ValueListenableBuilder(
-                      valueListenable: counter,
-                      builder: (context, score, child) {
-                        return Text(
-                          'Score: $score',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        );
-                      },
-                    ),
+          Stack(
+            children: [
+              GameWidget(game: game),
+              Positioned(
+                top: 50,
+                left: 20,
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.black54,
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                ),
-                Positioned(
-                  top: 50,
-                  right: 20,
-                  child: Row(
-                    children: [
-                      IconButton(
-                        icon: Icon(Icons.music_note, color: Colors.black),
-                        onPressed: () {},
-                      ),
-                      IconButton(
-                        icon: Icon(Icons.volume_up, color: Colors.black),
-                        onPressed: () {},
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(16),
-                        child: ElevatedButton(
-                          onPressed: () {
-                            counter.value++;
-                          },
-                          child: const Text("Tambah Score"),
+          
+                  child: ValueListenableBuilder(
+                    valueListenable: game.scoreNotifier,
+                    builder: (context, score, child) {
+                      return Text(
+                        'Score: $score',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
                         ),
-                      ),
-                    ],
+                      );
+                    },
                   ),
                 ),
-              ],
-            ),
+              ),
+              Positioned(
+                top: 50,
+                right: 20,
+                child: Row(
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.music_note, color: Colors.black),
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.volume_up, color: Colors.black),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ],
       ),
