0350fa63c0fa16803c325626ce4163baa29bf343
ZEINadli <1123150028@global.ac.id>
2026-02-19 20:01:08 +0700

    buat button tambah score

--- lib/main.dart
+++ lib/main.dart
@@ -35,45 +35,60 @@ class _GameScreenState extends State<GameScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
+      body: Column(
         children: [
-          Positioned(
-            top: 50,
-            left: 20,
-            child: Container(
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                color: Colors.black54,
-                borderRadius: BorderRadius.circular(10),
-              ),
-
-              child: ValueListenableBuilder(
-                valueListenable: counter,
-                builder: (context, score, child) {
-                  return Text(
-                    'Score: $score',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  );
-                }
-              ),
-            ),
-          ),
-          Positioned(
-            top: 50,
-            right: 20,
-            child: Row(
+          Expanded(
+            child: Stack(
               children: [
-                IconButton(
-                  icon: Icon(Icons.music_note, color: Colors.black),
-                  onPressed: () {},
+                Positioned(
+                  top: 50,
+                  left: 20,
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Colors.black54,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+            
+                    child: ValueListenableBuilder(
+                      valueListenable: counter,
+                      builder: (context, score, child) {
+                        return Text(
+                          'Score: $score',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        );
+                      },
+                    ),
+                  ),
                 ),
-                IconButton(
-                  icon: Icon(Icons.volume_up, color: Colors.black),
-                  onPressed: () {},
+                Positioned(
+                  top: 50,
+                  right: 20,
+                  child: Row(
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.music_note, color: Colors.black),
+                        onPressed: () {},
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.volume_up, color: Colors.black),
+                        onPressed: () {},
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            counter.value++;
+                          },
+                          child: const Text("Tambah Score"),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
